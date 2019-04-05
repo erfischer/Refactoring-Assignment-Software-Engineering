dc7cf58619e5e50aafd219d906c0a176e9d6a0e9
Evan Fischer <erfischer2@bsu.edu>
2019-04-04 23:11:56 -0400

    Split into declaration
    Split into declaration

--- src/ifs/InsuranceCalculator.java
+++ src/ifs/InsuranceCalculator.java
@@ -1,7 +1,7 @@
 package ifs;
 public class InsuranceCalculator {
 
-    private final InsuranceStrategyVeryHigh strategyVeryHigh = new InsuranceStrategyVeryHigh();
+    private InsuranceStrategyVeryHigh strategy;
 
     public double calculateInsurance(double income) {
 
@@ -13,8 +13,10 @@ public class InsuranceCalculator {
         } else if (income <= 60000) {
             return (income-30000)*0.1+76500;
         } else {
-            return strategyVeryHigh.calculateInsuranceVeryHigh(income);
+            strategy = new InsuranceStrategyVeryHigh();
+            return strategy.calculateInsuranceVeryHigh(income);
         }
 
     }
 
+}
